40d3eb79731db4b5dcc47fe1c267d611df744361
Simon Larsen <simonhffh@gmail.com>
2018-06-11 16:13:40 +0200

    Fixed documentation issues, added headers for non-ranger files

--- R/proximity.r
+++ R/proximity.r
@@ -1,3 +1,20 @@
+# -------------------------------------------------------------------------------
+# This file is part of Grand Forest.
+#
+# Grand Forest is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Grand Forest is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Grand Forest. If not, see <http://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------------------
+
 ##' @export
 proximity <- function(x, ...)  UseMethod("proximity")
 
@@ -7,9 +24,11 @@ proximity <- function(x, ...)  UseMethod("proximity")
 ##'
 ##' @title Grand Forest sample proximity
 ##' @param x grandforest.prediction object.
-##' @return "dist" object 
+##' @param ... Further arguments passed to or from other methods.
+##' @return Numeric matrix with sample similarities.
+##' @aliases proximity
 ##' @export
-proximity.grandforest.prediction <- function(x) {
+proximity.grandforest.prediction <- function(x, ...) {
   if(class(x) != "grandforest.prediction") {
       stop("Error: Input object to grandforest.prediction object.")
   }
--- R/unsupervised.R
+++ R/unsupervised.R
@@ -1,3 +1,20 @@
+# -------------------------------------------------------------------------------
+# This file is part of Grand Forest.
+#
+# Grand Forest is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Grand Forest is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Grand Forest. If not, see <http://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------------------
+
 ##' Train unsupervised Grand Forest model.
 ##' 
 ##' Helper function for training Grand Forest model for unsupervised analysis.
@@ -8,6 +25,7 @@
 ##' @param graph_data Feature interaction graph. Must be two-column character \code{data.frame} or \code{matrix} with character strings corresponding to column names in \code{data}.
 ##' @param replace Should background sampling be with replacement?
 ##' @param importance Variable importance mode, one of 'none', 'impurity', 'impurity_corrected', 'permutation'. The 'impurity' measure is the Gini index.
+##' @param ... Other arguments to be passed to the \code{grandforest} function during training.
 ##' @export
 grandforest_unsupervised <- function(data, graph_data=NULL, replace=TRUE, importance="impurity", ...) {
   bg <- apply(data, 2, function(x) sample(x, length(x), replace))
